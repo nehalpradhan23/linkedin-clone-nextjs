ae967e515d70eebd0f46b49628077904dd0448ef
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-05-27 23:53:35 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* toaster */}
+        <header>{/* header */}</header>
+        <div className="">
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
