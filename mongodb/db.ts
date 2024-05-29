c8449fcae084df21b65d3b189e23b6748441f7b5
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-05-29 20:06:24 +0530

    server actions, schemas and models

---
+++ mongodb/db.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const connectionString = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@linkedin-clone-nextjs.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000`;
+
+if (!connectionString) {
+  throw new Error("Provide a valid connection string");
+}
+
+// connect to database
+const connectDb = async () => {
+  // check if already connected
+  if (mongoose.connection?.readyState >= 1) {
+    return;
+  }
+
+  try {
+    console.log("======= Connecting to MongoDB =========");
+    await mongoose.connect(connectionString);
+  } catch (error) {
+    console.log("Error connecting to MongoDB: ", error);
+  }
+};
